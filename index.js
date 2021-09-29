434a717b169e023e677f57f132ee57a8795c4823
khalia <khaliaparris01@gmail.com>
2021-09-29 14:49:43 +0000

    completed code challenge

--- index.js
+++ index.js
@@ -1,25 +1,46 @@
 //1
 function countToTen(){
-  
+  for(let i = 1; i<=10; i++){
+      console.log(i)
+  }
 }
+//countToTen()
 
 //2
-function countFromOne(){
-  
+function countFromOne(num){
+    for(let i =1; i<=num;i++){
+    console.log(i)
+    }
 }
-
+//countFromOne(10)
 //3
-function countEveryEven(){
+function countEveryEven(num){
+    for(let i =0; i<=num; i+=2){
+        console.log(i)
+    }
   
 }
-
+//countEveryEven(6)
 //4
-function countEveryOdd(){
-  
+function countEveryOdd(num){
+  for(let i = 1; i <=num; i++){
+      if(i%2 != 0){
+       console.log(i)   
+      }
+      
+  }
 }
-
+//countEveryOdd(22)
 //5 
-function countEvens(){
-  
+function countEvens(num){
+   let count = 0
+   for (let i = 0; i <num.length; i++){
+       if (num[i]%2 ===0){
+           count++
+       }
+   }
+        
+  return  count
 }
 
+console.log(countEvens([1,2,3,4,5,6]))
